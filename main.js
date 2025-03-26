6849710966ea2527b81edd31a230283e890819b7
saidovanvar <saidovanvar@gmail.com>
2025-03-26 12:19:55 +0500

    uyga vazifa

---
+++ main.js
@@ -0,0 +1,68 @@
+// // 1-masala
+// let a = 0
+// let b = 100
+//
+// for (let i = a; i <= b; i+=2) {
+//     console.log(i)
+// }
+
+// while bilan
+// let i;
+// i=a;
+// while(i<=b){
+//     console.log(i)
+//     i+=2;
+// }
+
+// // 2-masala
+// let a= prompt("a ga qiymatni kiriting");
+// let b= prompt("b ga qiymatni kiriting");
+// let c= prompt("c ga qiymatni kiriting");
+//
+// let d;
+//
+// d=a
+// a=c
+// c=d
+// console.log(a,b,c);
+
+
+// // 3-masala
+// for blan:
+// let a = prompt("a ni kiriting")*1;
+// let b = prompt("b ni kiriting")*1;
+// let c = 0
+//
+// for (let i = a; i <= b; i++) {
+//    c=c+i;
+// }
+// console.log(c);
+
+// while bilan :
+
+// let i;
+// i=a;
+// while(i <= b){
+//     c=c+i;
+//     i++
+// }
+// console.log(c);
+
+//
+// let N = prompt("N ni kiriting")*1;
+// let C = 1
+// for bilan :
+// for (let i = 1; i <= N; i++) {
+//     C*=i;
+// }
+// console.log(C);
+
+// while blan :
+// let i=1;
+// while(i <= N){
+//     C*=i;
+//     i++
+// }
+// console.log(C)
+
+
