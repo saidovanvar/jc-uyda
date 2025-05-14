db8dc483f2e00b355730321738cd9b18924d6415
saidovanvar <saidovanvar@gmail.com>
2025-05-14 13:34:46 +0500

    js2 uyda

--- main.js
+++ main.js
@@ -44,21 +44,116 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ///////////////////////////////////////
+//
+//
+// 1-masala
+// function main(a , b) {
+//     return a + b;
+// }
+//
+// console.log(main(1,2,));
+
+///////////////////////////////////
+//
+// 2-masala
+// const a = (ism) => {
+//   return `Salom, ${ism}`;
+// }
+// console.log(a("Anvar"));
+
+/////////////////////////////////////////
+// let a = prompt("1daan 100 bahoni kritin");
+// if (a >= 90) {
+//     console.log("A")
+// }else if (a >= 89 || a >= 80) {
+//     console.log("B")
+// }else if (a >= 79 || a >=70) {
+//     console.log("C")
+// }else{
+//     console.log("F")
+// }
+
+// ////////////////////////////////////////////
+// let body = document.querySelector("body")
+// let a =document.createElement('div');
+// a.appendChild(body)
+/////////////////////////////////////////////////////////////////
+////////////////////////////
+//
+// let a = [1, 2, 3];
+// a.splice(0, 2);
+// console.log(a)
+
+////////////////////////////////////
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let b = a.filter((c)=> c % 2 === 0);
+// console.log(b)
+// //////////////////////////////////////////
 ////////////////////////////////////////////////
-let n = prompt("Son kiriting:");
-n = parseInt(n);
 
 
-let juft = 0;
-let toq = 0;
 
-for (let i = 1; i <= n; i++) {
-    if (i % 2 === 0) {
-        juft++;
-    } else {
-        toq++;
-    }
-}
+// let number = [1, 2, 3];
+//
+// number.forEach(function(raqam) {
+//   console.log(raqam);
+// });
 
-console.log(`Juft sonlar : ${juft}`);
-console.log(`Toq sonlar  : ${toq}`);
+////////////////////////////////////////
+//
+// let a = document.querySelector('#img')
+//
+// a.addEventListener('click', () => {
+//   console.log("salom dunyo");
+// })
+
+//////////////////////////////////////////
+//
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+/////////////////////////////////////////////
+// 5-masala
+let a =document.querySelector('#img');
+a.addEventListener('click', ()=>{
+    alert("Hello World!");
+})
